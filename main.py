e120c6565be63ee9ea4445858bfa0b8f960da895
neil <neil@neildeMacBook-Pro.local>
2022-03-02 23:06:52 +0800

    [fix] 處理無法登入 nike 問題中

--- main.py
+++ main.py
@@ -2,37 +2,58 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-# url = "https://www.nike.com/tw/launch?cp=78087397488_search_%7Ctw%7CCore%2BBrand%2B-%2BGN%2B-%2BPure%2B-%2BXCategory%2B-%2BNike%2BTaiwan%2B-%2BTM%2B-%2BGeneral%2B-%2BCH_CH%2B-%2BPhrase%7CGOOGLE%7Cnike&s=upcoming"
-url = "https://www.nike.com/"
+url = "https://www.nike.com/tw/launch"
+# url = "https://www.nike.com/"
+# url = "https://www.google.com.tw"
+
 options = Options()
+# 不顯示「彈出的訊息」
 options.add_argument("--disable-notifications")
+# 不顯示「正受到自動測試軟體控制」
+options.add_experimental_option("excludeSwitches",['enable-automation'])
+# 使用偽造的 user-agent
+ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36"
+options.add_argument("user-agent={}".format(ua))
+
+
 
 # 指定 chrome driver
-chrome = webdriver.Chrome('./linux/chromedriver', chrome_options=options)
+chrome = webdriver.Chrome('./mac/chromedriver', chrome_options=options)
 # 設定尺寸
 chrome.set_window_size(1500, 1280)
 # 開啟畫面
 chrome.get(url)
 
-# btn = chrome.find_element_by_class_name("join-log-in")
+## google browser ##
+# chrome.find_element_by_class_name("gLFyf").send_keys("nike 預購")
+
+# btn=chrome.find_element_by_class_name("gNO89b")
 # btn.click()
+
+# result=chrome.find_element_by_class_name("LC20lb")
+# result.click()
+
+##################
+
+btn = chrome.find_element_by_class_name("join-log-in")
+btn.click()
 # btn = chrome.find_element_by_xpath("//button[@class='nav-btn'")
 # btn.click()
 # print(btn.get_attribute("class"))
 
 # 輸入帳號
-# email = chrome.find_element_by_name("emailAddress")
-# email.send_keys("neil605164@gmail.com")
+email = chrome.find_element_by_name("emailAddress")
+email.send_keys("neil605164@gmail.com")
 
-# # 輸入密碼
-# email = chrome.find_element_by_name("password")
-# email.send_keys("2.Gs5JEAbY@FVH5")
+# 輸入密碼
+email = chrome.find_element_by_name("password")
+email.send_keys("2.Gs5JEAbY@FVH5")
 
-# # 登入
-# chrome.find_element_by_css_selector("input[type='button']").click()
+# 登入
+chrome.find_element_by_css_selector("input[type='button']").click()
 
 # 等待秒後關閉
-time.sleep(5)
+time.sleep(600)
 
 # 關閉
 chrome.quit()
